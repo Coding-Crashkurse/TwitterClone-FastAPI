20689ffe2676da030d86a38326966d4dab278444
markus <datamastery87@gmail.com>
2023-01-06 13:45:45 +0100

    add requirements.txt

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10
+
+COPY . /app
+WORKDIR app
+
+RUN pip install -r requirements.txt
+RUN pip install .
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "5555"]
--- src/app/db_and_models/engine.py
+++ src/app/db_and_models/engine.py
@@ -1,5 +1,10 @@
 from sqlmodel import create_engine
+import os
 
-engine = create_engine(
-    "sqlite:///twitter.db", connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     "sqlite:///twitter.db", connect_args={"check_same_thread": False}
+# )
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
