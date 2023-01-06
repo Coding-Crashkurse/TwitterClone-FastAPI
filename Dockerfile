d5e665d473aa3bcec7ebcd38e6dfcdc38c5ae391
markus <datamastery87@gmail.com>
2023-01-06 16:09:26 +0100

    remove data

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10
 
 COPY . /app
-WORKDIR app
+WORKDIR /app
 
 RUN pip install -r requirements.txt
 RUN pip install .
--- src/app/db_and_models/engine.py
+++ src/app/db_and_models/engine.py
@@ -1,10 +1,9 @@
-from sqlmodel import create_engine
 import os
+
+from sqlmodel import create_engine
 
 # engine = create_engine(
 #     "sqlite:///twitter.db", connect_args={"check_same_thread": False}
 # )
 
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(os.environ.get("DATABASE_URL"))
